257993b094bee721b3d5d8379a987c6847054306
Joseph Knoebel <knoebelja@gmail.com>
2017-08-15 17:35:43 -0400

    we are making that list yo

--- public/js/days.js
+++ public/js/days.js
@@ -1,4 +1,5 @@
 $(function populateSelects() {
+
   hotels.forEach(function(hotel) {
     $('#hotel-choices').append($('<option>', { value: hotel.id, text: hotel.name }))
   })
@@ -9,11 +10,36 @@ $(function populateSelects() {
     $('#activity-choices').append($('<option>', { value: activity.id, text: activity.name }))
   })
 
-  let value;
+  let idVal;
+  let place, myType, tableBoi, hName;
   $('#options-panel button').on('click', function() {
-    value = $(this).prev().val();
-    console.log(value)
+    idVal =  $(this).prev().val();
+
+    myType = $(this).prev().data('type');
+
+    // we get the header name
+    if (myType === 'hotel'){
+      tableBoi = hotels
+      hName = 'My Hotel'
+    } else if (myType === 'restaurant') {
+      tableBoi = restaurants
+      hName = 'My Restaurants'
+    } else {
+      tableBoi = activities
+      hName = 'My Activities'
+    }
+    // we get the place data
+    place = tableBoi.filter((hotel) => {
+      return hotel.id == idVal;
+    })
+
+    // insert data into next sibling of HEader name
+
+
+    console.log(myType, place);
+
+
+
   })
 
-  // function itenItem
 })
